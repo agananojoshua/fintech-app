40fe382ff6bebe93a8ff4d6380d87c618faf85e3
aganano joshua <agananojoshua001@gmail.com>
2024-06-07 13:08:54 +0100

    Implement Plaid LInk and dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,7 +22,8 @@ import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { getLoggedInUser } from '@/lib/actions/user.actions';
+import { getLoggedInUser, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
     const router = useRouter()
@@ -40,11 +41,26 @@ const AuthForm = ({ type }: { type: string }) => {
         },
     })
 
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
         setIsLoading(true)
 
         if(type === 'sign-up'){
-            
+            const userData = {
+                firstName: data.firstName!,
+                lastName: data.lastName!,
+                address1: data.address1!,
+                city: data.city!,
+                state: data.state!,
+                postalCode: data.postalCode!,
+                dateOfBirth: data.dateOfBirth!,
+                ssn: data.ssn!,
+                email: data.email,
+                password: data.password
+              }
+    
+              const newUser = await signUp(userData);
+    
+              setUser(newUser);
         }
 
         if(type === 'sign-in'){
@@ -52,7 +68,7 @@ const AuthForm = ({ type }: { type: string }) => {
         }
 
         try {
-            console.log(values)
+            console.log(data)
             setIsLoading(false)
         } catch (error) {
             console.log(error)
@@ -92,7 +108,7 @@ const AuthForm = ({ type }: { type: string }) => {
             {user ?
                 (
                     <div className='flex flex-col gap-4'>
-                        {/* PlaidLink */}
+                        <PlaidLink user={user} variant="primary"/>
                     </div>
                 ) : (
                     <>
