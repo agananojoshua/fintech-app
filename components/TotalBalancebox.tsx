b5ca61016c146f8aac9fe2df3497f936d09b5653
aganano joshua <agananojoshua001@gmail.com>
2024-06-11 13:07:51 +0100

    last

--- components/TotalBalancebox.tsx
+++ components/TotalBalancebox.tsx
@@ -20,7 +20,7 @@ const TotalBalanceBox = ({
           </p>
 
           <div className="total-balance-amount flex-center gap-2">
-            <AnimatedCounter amount={totalCurrentBalance}
+            <AnimatedCounter amount={totalCurrentBalance} />
           </div>
         </div>
       </div>
